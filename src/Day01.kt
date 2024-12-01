a7faf889d149166d84ace2a21c083951f2b2f2d4
Andrew <vmpay.eu@gmail.com>
2024-12-01 18:57:37 +0100

    Part one impl

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,8 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return dayOnePartOne(input)
     }
 
     fun part2(input: List<String>): Int {
@@ -8,14 +10,29 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    check(part1(listOf("3   14")) == 11)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun dayOnePartOne(lines: List<String>): Int {
+    val (left, right) = lines.map { line ->
+        val first = line.substringBefore(" ").toInt()
+        val second = line.substringAfter(" ").replace(" ", "").toInt()
+        first to second
+    }.unzip()
+
+    val result = left.sorted()
+        .zip(right.sorted())
+        .sumOf { (first, second) ->
+            abs(first - second)
+        }
+    return result
 }
