bc4aa055f0d5ab06df8d430f7d8ffa94f8fb386f
Andrew <vmpay.eu@gmail.com>
2024-12-06 23:55:12 +0100

    Day05: template impl

---
+++ src/Day05.kt
@@ -0,0 +1,19 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val inputTest = readInput("Day04_test")
+
+    check(part1(inputTest) == 18)
+    check(part2(inputTest) == 9)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
+
